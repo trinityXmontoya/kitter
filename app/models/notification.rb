e2b78a4a3023faee0ff339f13ce17bc928c035b5
trinityXmontoya <trinityXmontoya@gmail.com>
2014-06-08 23:53:48 -0400

    working replies

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,11 +2,4 @@ class Notification < ActiveRecord::Base
   belongs_to :tweet, foreign_key: 'tweet_id', class_name: 'Tweet'
   belongs_to :user, foreign_key: 'user_id', class_name: 'User'
   belongs_to :poster, foreign_key: 'poster_id', class_name: 'User'
-
-  after_create :mark_unread
-
-  def mark_unread
-    update(read: false)
-  end
-
 end
