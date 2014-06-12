469391c3cfb03ed444faa1fc10e43804404d9373
trinityXmontoya <trinityXmontoya@gmail.com>
2014-06-12 15:21:03 -0400

    heroku deploy basic setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Kitter::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-config.assets.precompile += [ 'session/login.js users/edit.js users/sign_up.js' ]
+config.assets.precompile += [ 'rails_admin.js session/login.js users/edit.js users/sign_up.js' ]
 
   # Code is not reloaded between requests.
   config.cache_classes = true
